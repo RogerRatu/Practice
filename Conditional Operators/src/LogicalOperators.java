4f21ae5a94465baf5fbdb0ff73461784064d7476
RogerRatu <rodgaratu@windowslive.com>
2019-05-18 14:44:26 +1200

    Initial commit 18/05/2019

---
+++ Conditional Operators/src/LogicalOperators.java
@@ -0,0 +1,46 @@
+
+public class LogicalOperators {
+
+	public static void main(String[] args) {
+		
+		boolean a = (2<1);
+        
+        
+		boolean b = false;
+                        
+                            
+		
+                        
+                            
+		// Logical AND
+                        
+                            
+		System.out.println("(a && b) = " + (a && b));
+                        
+                            
+		// Logical OR
+                        
+                            
+		System.out.println("(a || b) = " + (a || b));
+                        
+                            
+		// Logical NOT
+                        
+                            
+		System.out.println("!(a) = " + !(a));
+                        
+                            
+		System.out.println("!(b) = " + !(b));
+                        
+                            
+		System.out.println("!(a && b) = " + !(a && b));
+                        
+                            
+	}
+                        
+                            
+ 
+                        
+                            
+}
+                        
